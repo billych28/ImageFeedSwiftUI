202bb656d93f849a3aaa0e74ae39ab915ee4c4a2
Мамытов Руслан <rmamytov@sberbank.ru>
2026-06-15 21:45:59 +0700

    Initial Commit

---
+++ ImageFeedSwiftUI/ImageFeedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedSwiftUIApp.swift
+//  ImageFeedSwiftUI
+//
+//  Created by Мамытов Руслан on 15.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct ImageFeedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
